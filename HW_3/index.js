af69d3f047efb73713d32019ba05283cd62e5b48
Nikita <nikitadave842@gmail.com>
2023-03-23 23:58:28 +0100

    HW_3

---
+++ HW_3/index.js
@@ -0,0 +1,17 @@
+// 3.1
+const firstStr = prompt("Введіть перше речення:")
+const secondStr = prompt("Введіть друге речення:")
+const thirdStr = prompt("Введіть третє речення:")
+
+alert(`${firstStr} 
+${secondStr} 
+${thirdStr}`)
+
+// 3.2
+const num = '12345'
+const splitNum = num.split('')
+console.log(splitNum)
+const joinedNum = splitNum.join(' ')
+console.log(joinedNum)
+
+
