193c6edf8e7e06cd6cb5871e59d9a0f7d15d19d5
Nikita <nikitadave842@gmail.com>
2023-03-26 19:57:29 +0200

    HW_7

---
+++ HW_7/index.js
@@ -0,0 +1,11 @@
+let num1 = prompt('Введіть перше число: ')
+let num2 = prompt('Введіть друге число: ')
+let num3 = prompt('Введіть третє число: ')
+
+num1 = parseFloat(num1)
+num2 = parseFloat(num2)
+num3 = parseFloat(num3)
+
+let result = (num1 + num2 + num3) / 3
+
+alert(`${result}`) 
