fb8b8b1e861840ac6197dccc585f58c7bd999fc6
Nikita <nikitadave842@gmail.com>
2023-04-02 23:09:12 +0200

    HW_11

---
+++ HW_11/index.js
@@ -0,0 +1,51 @@
+//11.1
+// let arr = []
+// for (let i = 20; i <= 30; i += 0.5) {
+//     arr.push(i)
+// }
+// console.log(arr)
+
+//11.2
+// let uah = 27
+// for (let dollars = 10; dollars <= 100; dollars += 10) {
+//     console.log(dollars * uah)
+// }
+
+//11.3
+// let num = 88
+// for (let i = 1; i <= 100; i++) {
+//     if (i ** 2 <= num) {
+//         console.log(i)
+//     }
+// }
+
+//11.4
+// let num = 10;
+// let simple = true;
+
+// for (let i = 2; i < num; i++) {
+//     if (num % i == 0) {
+//         simple = false;
+//     }
+// }
+
+// if (simple) {
+//     console.log(num + " є простим числом");
+// } else {
+//     console.log(num + " не є простим числом");
+// }
+
+//11.5
+// let num = 9;
+// let check = false;
+
+// if (num > 0) {
+//     while (num % 3 === 0) {
+//         num /= 3;
+//     }
+//     if (num === 1) {
+//         check = true;
+//     }
+// }
+
+// console.log(isPowerOfThree); 
