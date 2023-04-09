a89563b4250f3d6f70d9b063bfe011139a240b27
Nikita <nikitadave842@gmail.com>
2023-04-09 23:45:49 +0200

    HW_16

---
+++ HW_16/index.js
@@ -0,0 +1,12 @@
+function generateKey(length, characters) {
+    let result = '';
+    const charactersLength = characters.length;
+    for (let i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+}
+
+const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+const key = generateKey(16, characters);
+console.log(key);
