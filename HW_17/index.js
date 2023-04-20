cbfa4102bb223ed071a38e3054cc5dd0c20fb2cc
Nikita <nikitadave842@gmail.com>
2023-04-20 20:50:21 +0200

    HW_20

--- HW_17/index.js
+++ HW_17/index.js
@@ -1,9 +1,8 @@
 //17.1
+// const arr = [2, 'Hello', 6, 'asd', 5]
 // function averageNumericElements(arr) {
 //     let sum = 0;
 //     let count = 0;
-//     arr = [2, 'Hello', 6, 'asd', 5]
-
 //     for (let i = 0; i < arr.length; i++) {
 //         if (typeof arr[i] === 'number') {
 //             sum += arr[i];
@@ -14,10 +13,10 @@
 //     return count === 0 ? 0 : sum / count;
 // }
 
-// const result = averageNumericElements()
+// const result = averageNumericElements(arr)
 // console.log(result)
 
-//17.2
+// 17.2
 // function doMath(x, znak, y) {
 //     if (znak == '+') {
 //         return x + y;
@@ -36,6 +35,21 @@
 //     }
 // }
 
+
+// switch(znak) {
+//     case '+':  // if (x === 'value1')
+//       return x + y
+//       [break]
+  
+//     case 'value2':  // if (x === 'value2')
+//       ...
+//       [break]
+  
+//     default:
+//       ...
+//       [break]
+//   }
+
 // const result = doMath(2, 'asd', 2)
 // console.log(result)
 
@@ -44,9 +58,8 @@
 // let cols = prompt("Введіть кількість стовпців:");
 
 // function fillArray(rows, cols) {
-//     let arr = [];
+//     const arr = [];
 //     for (let i = 0; i < rows; i++) {
-//         arr[i] = [];
 //         for (let j = 0; j < cols; j++) {
 //             arr[i][j] = prompt(`Введіть значення для елементу (${i}, ${j}):`);
 //         }
@@ -59,23 +72,23 @@
 // alert(myArray);
 
 //17.4
-function remove(str, charsToRemove) {
-    let result = "";
-    for (let i = 0; i < str.length; i++) {
-        let charToRemove = false;
-        for (let j = 0; j < charsToRemove.length; j++) {
-            if (str[i] === charsToRemove[j]) {
-                charToRemove = true;
-                break;
-            }
-        }
-        if (!charToRemove) {
-            result += str[i];
-        }
-    }
-    return result;
-}
-const str = " hello world";
-const charsToRemove = ['e', 'd'];
-const result = remove(str, charsToRemove);
-console.log(result); 
+// function remove(str, charsToRemove) {
+//     let result = "";
+//     for (let i = 0; i < str.length; i++) {
+//         let charToRemove = false;
+//         for (let j = 0; j < charsToRemove.length; j++) {
+//             if (str[i] === charsToRemove[j]) {
+//                 charToRemove = true;
+//                 break;
+//             }
+//         }
+//         if (!charToRemove) {
+//             result += str[i];
+//         }
+//     }
+//     return result;
+// }
+// const str = " hello world";
+// const charsToRemove = ['e', 'd'];
+// const result = remove(str, charsToRemove);
+// console.log(result); 
