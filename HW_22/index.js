92f188d26c23ff391f6acee91b6da129ea756dd7
Nikita <nikitadave842@gmail.com>
2023-04-23 18:08:25 +0200

    HW_22

---
+++ HW_22/index.js
@@ -0,0 +1,40 @@
+class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    info() {
+        console.log(`${this.name}, ${this.age}`)
+    }
+}
+
+class Car {
+    constructor(brand, model, year, carNum) {
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+        this.carNum = carNum;
+        this.owner = "";
+    };
+    assignOwner(person) {
+        if (person.age >= 18) {
+            this.owner = person;
+        } else {
+            console.log("Людині менше 18 років.")
+        }
+    };
+    printInfo() {
+        console.log(`Car: ${this.brand} ${this.model}, Year: ${this.year}, CarNum: ${this.carNum}`);
+        this.owner.info()
+    };
+}
+
+
+const person1 = new Person('Alex', 18)
+const person2 = new Person('Max', 23)
+const car = new Car("BMW", "X5", 2022, "KA9999CC")
+const car1 = new Car("BMW", "M8", 2021, "KA6666CC")
+car.assignOwner(person1)
+car1.assignOwner(person2)
+car.printInfo()
+car1.printInfo()
