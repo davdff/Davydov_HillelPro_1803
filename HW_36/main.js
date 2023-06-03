787f0f1088efe7b2bf3ee60e563f2fda374ae63f
Nikita <nikitadave842@gmail.com>
2023-06-03 21:38:16 +0200

    HW_36

---
+++ HW_36/main.js
@@ -0,0 +1,59 @@
+const http = require('http');
+const fs = require('fs');
+function getRandNum(min, max) {
+    return (Math.random() * (max - min) + min).toFixed(4)
+}
+
+const createOffer = new Array(25).fill(null).map((_, i) => ({
+    id: i,
+    name: `Offer${i}`,
+    coordinate: [getRandNum(36.0000, 37.5000), getRandNum(36.0000, 37.5000)]
+}))
+
+fs.writeFileSync('offers.txt', JSON.stringify(createOffer));
+
+http.createServer(function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+
+    const url = req.url;
+    const method = req.method;
+
+    if (method === 'POST') {
+        if (url === '/add-offer') {
+            let body = '';
+
+            req.on('data', (chunk) => {
+                body += chunk;
+            });
+
+            req.on('end', () => {
+                // Получение предложения из тела запроса
+                const newOffer = JSON.parse(body);
+
+                // Добавление предложения к существующим предложениям
+                const offers = JSON.parse(fs.readFileSync('offers.txt', 'utf8'));
+                offers.push(newOffer);
+
+                // Сохранение обновленного списка предложений
+                fs.writeFileSync('offers.txt', JSON.stringify(offers));
+
+                res.write('Offer added successfully');
+                res.end();
+            });
+        } else {
+            res.write('wrong route');
+            res.end();
+        }
+    } else if (method === 'GET') {
+        if (url === '/offers') {
+            const data = fs.readFileSync('offers.txt', 'utf8');
+            res.write(data)
+            res.end()
+        } else {
+            res.write('wrong route');
+            res.end();
+        }
+    }
+}).listen(4001, function () {
+    console.log("server start at port 4001");
+});
